2ce3acca512739fee3392261fa074b3f678977c8
Targema Charles <targemac@gmail.com>
2025-11-27 06:42:33 +0100

    completed user-input

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello bro");
-        System.out.println("learning something new");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+//        to read a string
+        String name = scanner.nextLine();// this reads a string of characters including spaces
+        System.out.println("Hello " + name);
+
+//        to read an integer
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        System.out.println("You are: " + age + " years old");
+
+//        to read a double
+        System.out.print("What is your GPA ?");
+        double gpa = scanner.nextDouble();
+        System.out.println("Your gpa is: " + gpa);
+
+//        to read a boolean
+        System.out.print("Are you a student ? (true/false)");
+        boolean isStudent = scanner.nextBoolean();
+        if(isStudent){
+        System.out.println("you are a student");
+        }else{
+            System.out.println("you are not a student");
+        }
+
+//        common issue
+//fix issues with calling scanner.nextLine() method
+
+        scanner.close();
     }
 }
