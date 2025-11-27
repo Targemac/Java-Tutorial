93349e4db9bd268d47f25aba569454cebf22ca3b
Targema Charles <targemac@gmail.com>
2025-11-27 05:00:49 +0100

    added a tip

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.print("Hello bro");
-        System.out.print("Hello bro");
+        System.out.println("Hello bro");
+        System.out.println("learning something new");
     }
 }
