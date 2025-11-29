de9baa46bbecc14f03738c5c8ec3fe6832f2494b
Targema Charles <targemac@gmail.com>
2025-11-29 06:49:18 +0100

    completed random-numbers

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello bro");
-        System.out.println("learning something new");
+       Random random = new Random();
+
+       int number1;
+       double number2;
+       int number3;
+
+       number1 = random.nextInt(1,101);
+       number2 = random.nextDouble(1,101);
+       number3 = random.nextInt(1,101);
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
     }
 }
