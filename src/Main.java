fe76147fcb1239b6596fd4da6d192b4c22dda467
Targema Charles <targemac@gmail.com>
2025-12-01 01:34:58 +0100

    completed nested if statements

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello bro");
-        System.out.println("learning something new");
+
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 9.99;
+
+        if(isStudent){
+            if(isSenior){
+            System.out.println("You get a senior discount of 20%");
+            System.out.println("You get a student discount of 10%");
+            price = price * 0.7;
+
+            }else{
+            System.out.println("You get a student discount of 10%");
+            price = price * 0.9;
+            }
+        }else{
+            price = price * 1;
+        }
+
+        System.out.printf("The price of a ticket is: NGN%.2f", price);
     }
 }
