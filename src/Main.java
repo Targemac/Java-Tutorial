abb03acfc81201890c36b6e14a14468d89de77b5
Targema Charles <targemac@gmail.com>
2025-12-01 01:17:09 +0100

    completed compound interest calculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello bro");
-        System.out.println("learning something new");
+//        compound interest calculator
+        Scanner scanner = new Scanner(System.in);
+
+        double principal;
+        double rate;
+        int timesCompound;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+       principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the number of times compounded per year: ");
+        timesCompound = scanner.nextInt();
+
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompound, timesCompound * years);
+
+        System.out.printf("The amount after %d years is NGN%,.2f", years, amount);
+
+
+        scanner.close();
     }
 }
