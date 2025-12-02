55f4d9dfced51e28d67f7eb1bcc6daf971740930
Targema Charles <targemac@gmail.com>
2025-12-03 00:25:39 +0100

    completed for-loops

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+//for loop = execute a certain amount of times
 
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter how many times you want to loop: ");
+//        int max = scanner.nextInt();
 
+//        for (int i = 1; i <= max; i++) {
+//            System.out.printf("%d Hey\n", i);
+//        }
+
+        int start = 10;
+
+        for (int i = start; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("HAPPY NEW YEAR!");
+
+//        scanner.close();
     }
 }
