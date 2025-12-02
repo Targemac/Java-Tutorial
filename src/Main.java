c87a010ce4b73f4b2dc77296c1d3415b5e7ec2a0
Targema Charles <targemac@gmail.com>
2025-12-02 05:25:27 +0100

    completed substrings

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
-public class Main {
-    public static void main(String[] args){
+import java.util.Scanner;
 
-        System.out.println("Hello bro");
-        System.out.println("learning something new");
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String username;
+        String domain;
+
+        System.out.println("Enter your email: ");
+        email = scanner.nextLine();
+
+        if (email.contains("@")) {
+            username = email.substring(0, email.indexOf("@"));
+            domain = email.substring(email.indexOf("@") + 1);
+
+
+            System.out.println(username);
+            System.out.println(domain);
+        } else {
+            System.out.println("Emails must contain @");
+        }
+
+
+        scanner.close();
     }
 }
