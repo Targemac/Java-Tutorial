5de8880d0e6bfb61d635ea4053262dfa5961b6d1
Targema Charles <targemac@gmail.com>
2025-12-02 22:31:36 +0100

    completed while-loops

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String name = "";
+
+        while (name.isEmpty()) {
+            System.out.print("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        System.out.printf("\nHello %s", name);
+
+        scanner.close();
 
     }
 }
