aaa94ad7694db6d4e854b177f0a03a799f459607
Targema Charles <targemac@gmail.com>
2025-12-02 06:11:56 +0100

    completed ternary-operator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        System.out.println("Hello bro");
-        System.out.println("learning something new");
+        int score = 70;
+        if (score >= 60) {
+            System.out.println("PASS");
+        } else {
+            System.out.println("FAIL");
+        }
+
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        System.out.println(passOrFail);
+
+        int number = 3;
+        String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
+        System.out.println(evenOrOdd);
+
+        int hours = 13;
+        String timeOfDay = (hours < 12) ? "AM" : "PM";
+        System.out.println(timeOfDay);
+
+        int income = 60000;
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+        System.out.printf("%,.2f", taxRate);
+
     }
 }
