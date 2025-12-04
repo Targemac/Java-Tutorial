38e5eddf68a8b4c40eaab96f41543f682784df03
Targema Charles <targemac@gmail.com>
2025-12-04 10:24:20 +0100

    completed variable-scope

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
+
+    static int x = 3;//class scope
+
+
     public static void main(String[] args) {
 
+        int x = 1;//Local scope
+        doSomething();
 
+    }
+
+    static void doSomething() {
+//        int x = 2;//local scope
+
+        System.out.println(x);
     }
 }
