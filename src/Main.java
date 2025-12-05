42ebb31917f7f12cf183325d648cda8d30a14022
Targema Charles <targemac@gmail.com>
2025-12-05 13:53:10 +0100

    completed array-of-objects

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
+        //
+
+        Car car1 = new Car("Mustang", "Red");
+        Car car2 = new Car("corvette", "Blue");
+        Car car3 = new Car("charger", "yellow");
+
+        //        anonymous objects
+        //        Car[] cars = {
+        //                new Car("Mustang", "Red"),
+        //                new Car("corvette", "Blue"),
+        //                new Car("charger", "yellow"),
+        //        };
+
+        //                Car[] cars = new Car[3];
+        Car[] cars = {car1, car2, car3};
+
+        for (Car car : cars) {
+            car.color = "black";
+        }
+
+        for (Car car : cars) {
+            car.drive();
+            car.color = "black";
+        }
 
     }
 }
