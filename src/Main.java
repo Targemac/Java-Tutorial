1ddbd1f61b6a553d8be6452b5e610bcea215bec5
Targema Charles <targemac@gmail.com>
2025-12-05 16:09:56 +0100

    completed inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
+        //        inheritance = one class inherits the attributes and methods from another class.
+        //        Child <- Parent <- GrandParent
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Plant plant = new Plant();
+
+        dog.eat();
+
+        dog.speak();
+        cat.speak();
+
+        System.out.println(dog.isAlive);
+        System.out.println(cat.isAlive);
+
+        plant.photosynthesis();
+        System.out.println(plant.isAlive);
+
 
     }
 }
