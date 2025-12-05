25a7c1484ad4056ea13c198a91e8ab7b8ca73df9
Targema Charles <targemac@gmail.com>
2025-12-05 11:13:55 +0100

    completed constructors

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        //        constructor = a special method to initialize objects, you can pass arguments
+        //        to a constructor and setup initial values
+
+
+        Student student1 = new Student("charles", 30, 3.2);
+        Student student2 = new Student("patrick", 34, 1.2);
+
+        student1.study();
+
 
     }
 }
