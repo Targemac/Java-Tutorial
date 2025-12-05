ca24f60a34e165b79a542d30c88edcd2fe07661f
Targema Charles <targemac@gmail.com>
2025-12-05 16:54:33 +0100

    completed super

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        //super = refers to the parent class (subclass (aka child class) <- superclass (aka parent class))
+        //        used in constructors and method overriding
+        //        calls the parent constructor to initialize attributes
+
+
+        Person person = new Person("Tunde", "Nut");
+        Student student = new Student("Bola", "Ahmed", 4.3);
+        Employee employee = new Employee("Kate", "ola", 50000);
+
+        person.showName();
+        System.out.println(student.gpa);
+        student.showName();
+        student.showGpa();
+        employee.showSalary();
 
 
     }
