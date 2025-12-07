960bf21dac77c790ed93827e6dfd0926c67e3d1b
Targema Charles <targemac@gmail.com>
2025-12-07 14:51:36 +0100

    completed interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+         * Interface = a blueprint for a class that specifies a set of abstract methods that implementing clsses must define.
+         * supports multiple inheritance-like behaviour.
+         *
+         * a class can implement multiple interfaces
+         * */
+
+
+        Rabbit rabbit = new Rabbit();
+        Hawk hawk = new Hawk();
+        Fish fish = new Fish();
+
+        rabbit.flee();
+        hawk.hunt();
+
+        fish.flee();
+        fish.hunt();
 
     }
 }
