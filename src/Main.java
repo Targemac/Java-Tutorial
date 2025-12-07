e62096be29e41e948f61911b45b660b8a32bb274
Targema Charles <targemac@gmail.com>
2025-12-07 15:33:47 +0100

    completed getters-setters

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        /*
+         * They help protect object data and add rules for accessing or modifying them.
+         * GETTERS = methods that make a field readable
+         * SETTERS = methods that make a field writeable
+         * */
+
+        Car car = new Car("Charger", "Yellow", 10000);
+        car.setColor("white");
+        car.setPrice(-6533);
+        System.out.println(car.getColor() + " " + car.getModel() + " " + car.getPrice());
 
     }
 }
