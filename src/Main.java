647872c235e102ac906fde4355050b84ed4d458c
Targema Charles <targemac@gmail.com>
2025-12-07 13:21:38 +0100

    completed tostring-method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
+        //        .toString() = method inherited from the Object class.
+        //        used to return a string representation of an object
+        //        by default, it returns a hash code as a unique identifier.
+        //        it can be overridden to provide meaningful details
 
+
+        Car car1 = new Car("Ford", "Mustang", 2025, "Red");
+        Car car2 = new Car("Chevrolet", "corvette", 2029, "Blue");
+
+
+        System.out.println(car1);
+        System.out.println(car2);
     }
 }
