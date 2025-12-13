f6b71cbf4da7106b9cb6ab5b3841b201d540ddc4
Targema Charles <targemac@gmail.com>
2025-12-13 06:49:14 +0100

    completed hashmaps

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
+        /*
+         * Hashmap = a data structure that stores key-value pairs
+         * keys are unique, but values can be duplicated
+         * does not maintain any other, but is memory efficient
+         * HashMap<key, value>
+         * */
 
+        HashMap<String, Double> map = new HashMap<>();
+        map.put("apple", 0.50);
+        map.put("orange", 0.75);
+        map.put("banana", 0.69);
+
+        //        map.remove("apple");
+
+        System.out.println(map);
+        System.out.println(map.get("apple"));
+        System.out.println(map.containsKey("apple"));
+
+        if (map.containsKey("orange")) {
+            System.out.println(map.get("orange"));
+        } else {
+            System.out.println("Key not found");
+        }
+
+        System.out.println(map.containsValue(0.75));
+
+        System.out.println(map.size());
+
+        for (String key : map.keySet()) {
+            System.out.println(key + " : NGN" + map.get(key));
+        }
     }
 }
